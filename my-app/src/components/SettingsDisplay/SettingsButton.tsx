f332a2da17a5a6a847d77ed71b228bf785be6c59
goldenyak <goldenyakegor@gmail.com>
2021-08-22 22:05:09 +0300

    add some functions

--- my-app/src/components/SettingsDisplay/SettingsButton.tsx
+++ my-app/src/components/SettingsDisplay/SettingsButton.tsx
@@ -19,7 +19,7 @@ export function SettingsButton(props: SettingsButtonType) {
         <button className={(props.startInputValue === 0 ? "disabled_button" : "settings_button"
                             || props.displayValue >= props.startInputValue ? "disabled_button" : "settings_button"
                             || props.maxInputValue <= props.startInputValue ? "disabled_button" : "settings_button")}
-                disabled={props.startInputValue < 0 || props.startInputValue > props.maxInputValue}
+                disabled={props.startInputValue < 0 || props.startInputValue > props.maxInputValue || props.startInputValue === props.maxInputValue}
                 onClick={props.addValueToDisplay}>set</button>
 
     )
