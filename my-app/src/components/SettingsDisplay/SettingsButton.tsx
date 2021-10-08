f357cc44e6d3d85356170b82974ce36233955361
goldenyak <goldenyakegor@gmail.com>
2021-10-08 23:05:48 +0300

    update package.json and add library for deploy

--- my-app/src/components/SettingsDisplay/SettingsButton.tsx
+++ my-app/src/components/SettingsDisplay/SettingsButton.tsx
@@ -16,10 +16,7 @@ export function SettingsButton(props: SettingsButtonType) {
         //                     && props.maxInputValue <= props.startInputValue ? "disabled_button" : "settings_button")}
         //         onClick={props.addValueToDisplay}>set</button>
 
-        <button className={(props.startInputValue === 0 ? "disabled_button" : "settings_button"
-        || props.displayValue >= props.startInputValue ? "disabled_button" : "settings_button"
-        || props.maxInputValue <= props.startInputValue ? "disabled_button" : "settings_button"
-        || props.maxInputValue > props.startInputValue ? "settings_button" : "disabled_button")}
+        <button className={( props.maxInputValue > props.startInputValue ? "settings_button" : "disabled_button")}
                 disabled={props.startInputValue < 0
                 || props.startInputValue > props.maxInputValue
                 || props.startInputValue === props.maxInputValue
